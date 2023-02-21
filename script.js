7f0b2f8edede5e5a32e8d64f3bfc89f2638b4897
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-02-21 15:18:30 +0530

    game working

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
+
+
 
 function getComputersChoice(arr){
     return arr[Math.floor(Math.random()*arr.length)];
@@ -6,14 +10,38 @@ function getComputersChoice(arr){
 
 function playRound(pc, cc){
     pc = pc.toLowerCase();
-    if(pc===cc){return `Tie! Both players got ${pc}`}
-    if((pc==="rock" && cc==="paper")||(pc==="paper" && cc==="scissors")||(pc==="scissors" && cc==="rock")){
-        return `You Lose! ${cc} beats ${pc}`;
+    console.log(pc, cc)
+    if(pc===cc){
+        return 'tie';
     }
-    return `You Win! ${pc} beats ${cc}`
+    if((pc==="rock" && cc==="paper")||(pc==="paper" && cc==="scissors")||(pc==="scissors" && cc==="rock")){
+        return `lose`;
+    }
+    return `win`;
 }
+let buttons = document.querySelectorAll('button');
+buttons = [...buttons];
 
-const p = "paper"
-const c = getComputersChoice(choices)
-alert(playRound(p, c))
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const cc = getComputersChoice(choices)
+        document.querySelector("#playerChoice").textContent = `Player's Choice: ${button.id}`;
+        document.querySelector("#computerChoice").textContent = `Computer's Choice: ${cc}`;
+        let result = playRound(button.id, cc);
 
+        if (result === 'win'){
+            playerScore++
+            document.querySelector("#status").textContent = `You won this round!`
+        }
+        else if (result === 'lose'){
+            computerScore++
+            document.querySelector("#status").textContent = `You lost this round!`
+        }
+        else{
+            document.querySelector("#status").textContent = `This round is a tie!`
+        }
+
+        document.querySelector("#playerScore").textContent = `Player's score: ${playerScore}`;
+        document.querySelector("#computerScore").textContent = `Computer's score: ${computerScore}`;
+    })
+})
