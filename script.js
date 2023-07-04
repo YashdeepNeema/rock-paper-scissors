182cfbb02a52e93d1107ca67375cf1771e6d8bb0
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-07-04 12:32:34 +0530

    write playRound function

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ function getComputerChoice(choicesArr){
     let i = Math.floor(Math.random() * choicesArr.length)
     return choicesArr[i]
 }
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+
+    if (computerSelection == playerSelection) return `Tie!`
+
+    else if(computerSelection == 'paper'){
+        if(playerSelection == 'scissors') {
+            return `You Won! ${playerSelection} beats ${computerSelection}` 
+        }
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+
+    else if(computerSelection == 'scissors'){
+        if(playerSelection == 'rock') {
+            return `You Won! ${playerSelection} beats ${computerSelection}` 
+        }
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+
+    else if(computerSelection == 'rock'){
+        if(playerSelection == 'paper') {
+            return `You Won! ${playerSelection} beats ${computerSelection}` 
+        }
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
