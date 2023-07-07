6ba2b811fdcfa43bfacc351f743caa62331c54ae
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-07-07 11:47:02 +0530

    make the buttons function

--- script.js
+++ script.js
@@ -69,9 +69,57 @@ function game(n, playerSelection){
 
 const buttons = document.querySelectorAll('button')
 
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
 buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
-        const result = playRound(`${btn.id}`, getComputerChoice(choices))
-        alert(result)
+        const computerChoice = getComputerChoice(choices)
+        const result = playRound(`${btn.id}`, computerChoice)
+        
+        document.querySelector('#playerChoice').innerHTML = `Player's Choice: ${btn.id.toUpperCase()}`
+        document.querySelector('#computerChoice').innerHTML = `Computer's Choice: ${computerChoice.toUpperCase()}`
+
+
+        if (playerScore >= 5 || computerScore >= 5) {
+            if(playerScore >= 5){
+                
+                document.querySelector('#playerChoice').innerHTML = ''
+                document.querySelector('#computerChoice').innerHTML = ''
+                document.querySelector('#roundResult').innerHTML = `You Won!`
+                document.querySelector('#resultReason').innerHTML = ''
+            }
+            else{
+                
+                document.querySelector('#playerChoice').innerHTML = ''
+                document.querySelector('#computerChoice').innerHTML = ''
+                document.querySelector('#roundResult').innerHTML = `Computer Won!`
+                document.querySelector('#resultReason').innerHTML = ''
+            }
+        }
+        else{
+            if (result == 'W'){
+                playerScore++
+                document.querySelector('#roundResult').innerHTML = `You Won!`
+                document.querySelector('#resultReason').innerHTML = `${btn.id.toUpperCase()} beats ${computerChoice.toUpperCase()}`
+            }
+            else if (result == "L"){
+                computerScore++
+                document.querySelector('#roundResult').innerHTML = `You Lose!`
+                document.querySelector('#resultReason').innerHTML = `${computerChoice.toUpperCase()} beats ${btn.id.toUpperCase()}`
+            }
+            else{
+                tieScore++
+                document.querySelector('#roundResult').innerHTML = `Tie!`
+                document.querySelector('#resultReason').innerHTML = ''
+            }
+        }
+
+
+        
+
+        document.querySelector('#playerScore').innerHTML = `Player's Score: ${playerScore}`
+        document.querySelector('#computerScore').innerHTML = `Computer's Score: ${computerScore}`
     })
 })
